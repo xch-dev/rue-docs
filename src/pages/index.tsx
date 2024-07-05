82d61fc0bfa06baf94a13d4c426725b9642ea91b
Rigidity <me@rigidnetwork.com>
2024-07-05 18:53:33 -0400

    Change description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,12 +30,8 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout description="Documentation for the Rue programming language.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
